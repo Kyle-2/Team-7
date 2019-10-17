ca0473f9c901bedaaa911ec58ef601776ed989d1
Kid-Justice <drew.jennings@digipen.edu>
2019-10-16 17:26:00 -0700

    Scripts and code sorted and fixed

---
+++ Drew Jennings/Scripts/CreateEventFunction.cs
@@ -0,0 +1,27 @@
+﻿/* By: Ryan Scheppler
+ * Date: 10/9/19
+ * Description: Add to objects meant to create things on unity events
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEventFunction : MonoBehaviour
+{
+    public GameObject ObjectToMake;
+
+
+    //function can be run to create given object
+    public void CreateObject()
+    {
+        Instantiate(ObjectToMake, transform.position, transform.rotation);
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(ObjectToMake, transform.position, transform.rotation);
+    }
+}
